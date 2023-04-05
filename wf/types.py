989fb11260e4a3f7b2fcf45cc4a012396769686e
jvfe <jvfecav@gmail.com>
2023-04-05 07:44:03 -0300

    fix: Remove dataclass_json to allow registry

--- wf/types.py
+++ wf/types.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
 
-from dataclasses_json import dataclass_json
 from latch.types import LatchFile
 
 
@@ -28,7 +27,6 @@ class SpeciesAssignmentReference(Enum):
     RDP = "RDP"
 
 
-@dataclass_json
 @dataclass
 class Sample:
     read1: LatchFile
